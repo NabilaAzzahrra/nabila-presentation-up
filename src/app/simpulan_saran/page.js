6a0260404497e811ca551687081efb29f6f94afd
NabilaAzzahrra <zahrrranabill11@gmail.com>
2024-06-09 15:06:51 +0700

    finish

---
+++ src/app/simpulan_saran/page.js
@@ -0,0 +1,81 @@
+"use client";
+import { useState, useEffect } from "react";
+import { Roboto, Roboto_Mono } from "next/font/google";
+import Link from "next/link";
+import Image from "next/image";
+import MetodeAnimation from "../../../public/MetodeAnimation";
+import LottieAnimasi from "/public/LottieAnimation";
+
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+export const roboto_mono = Roboto_Mono({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export default function Halaman() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a loading delay
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000); // Adjust the delay as needed
+
+    return () => clearTimeout(timer);
+  }, []);
+  return (
+    <main
+      className={`${roboto.className} bg-white text-black px-96 flex h-screen items-center justify-center`}
+    >
+      {/* {loading && (
+        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center z-10 bg-black bg-opacity-80">
+          <LottieAnimasi />
+        </div>
+      )} */}
+      <div className="">
+        <div className="pb-3 font-extrabold text-xl flex items-center justify-center gap-5">
+          <div className="font-extrabold text-3xl flex items-end justify-center">
+            SIMPULAN DAN SARAN
+          </div>
+          <div className="font-extrabold text-xl flex items-center justify-center">
+            <MetodeAnimation />
+          </div>
+        </div>
+        <div className="mt-5">
+          <div className="mb-4">
+            <div className="font-bold text-xl mb-3 flex items-center justify-center">
+              Simpulan
+            </div>
+            <div className="mb-3 text-xl text-center text-wrap">
+            Aplikasi Web Sistem Informasi Relasi Perusahaan (SiRP) menggunakan codeigniter untuk menangani proses pengolahan data perusahaan. Aplikasi dibangun menggunakan database MySQL. Aplikasi sangat efisien untuk pengembangan sebagai cross platform. Kemudian terdapat beberapa fitur yang masih belum maksimal dan masih dapat dikembangkan lebih jauh.
+            </div>
+          </div>
+          <div className="">
+            <div className="font-bold text-xl mb-3 flex items-center justify-center">
+              Saran
+            </div>
+            <div className="mb-3 text-xl text-center text-wrap">
+            Karena aplikasi Sistem Informasi Relasi Perusahaan (SiRP) ini masih sederhana, maka penulis menyarankan untuk dapat dilakukan pengembangan lebih lanjut dikarenakan terdapat beberapa fitur yang belum tersedia seperti update profile, laporan triwulan, target penempatan, serta dari segi tampilan supaya lebih baik lagi. Dikarenakan penulis tidak menggunakan framework versi terbaru, penulis menyarankan untuk menggunakan framework lain yang memiliki hasil dan kinerja yang lebih baik daripada framework yang digunakan oleh penulis.
+            </div>
+          </div>
+        </div>
+        <div className="flex gap-2 mt-7">
+          <Link href={`/use_case_usulan`}>
+            <div className="h-10 w-10 bg-sky-400 rounded-full flex items-center justify-center">
+              <div className="triangle-left"></div>
+            </div>
+          </Link>
+          <Link href={`/selesai`}>
+            <div className="h-10 w-10 bg-sky-400 rounded-full flex items-center justify-center">
+              <div className="triangle-right"></div>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ src/app/use_case_usulan/page.js
@@ -0,0 +1,73 @@
+"use client";
+import { useState, useEffect } from "react";
+import { Roboto, Roboto_Mono } from "next/font/google";
+import Link from "next/link";
+import Image from "next/image";
+import MetodeAnimation from "../../../public/MetodeAnimation";
+import LottieAnimasi from "/public/LottieAnimation";
+
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+export const roboto_mono = Roboto_Mono({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export default function Halaman() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a loading delay
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000); // Adjust the delay as needed
+
+    return () => clearTimeout(timer);
+  }, []);
+  return (
+    <main
+      className={`${roboto.className} bg-black text-white px-96 flex h-screen items-center justify-center`}
+    >
+      {/* {loading && (
+        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center z-10 bg-black bg-opacity-80">
+          <LottieAnimasi />
+        </div>
+      )} */}
+      <div className="">
+        <div className="pb-3 font-extrabold text-xl flex items-center justify-center gap-5">
+          <div className="font-extrabold text-3xl flex items-end justify-center">
+            USE CASE USULAN
+          </div>
+          <div className="font-extrabold text-xl flex items-center justify-center">
+            <MetodeAnimation />
+          </div>
+        </div>
+        <div className="mt-5">
+          <div className="flex items-center justify-center">
+            <Image
+              src="/img/use_case.png"
+              alt="alt"
+              width={600}
+              height={600}
+            />
+          </div>
+        </div>
+        <div className="flex gap-2 mt-7">
+          <Link href={`/hasil_penelitian_pembahasan`}>
+            <div className="h-10 w-10 bg-sky-400 rounded-full flex items-center justify-center">
+              <div className="triangle-left"></div>
+            </div>
+          </Link>
+          <Link href={`/simpulan_saran`}>
+            <div className="h-10 w-10 bg-sky-400 rounded-full flex items-center justify-center">
+              <div className="triangle-right"></div>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
+}
